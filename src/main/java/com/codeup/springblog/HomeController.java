df3fa153cde20d7b5f6fcd7f9efac22fc4878df4
terryhale <aggiefan17@yahoo.com>
2018-01-18 15:12:15 -0600

    finished controllers exercise

---
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String landingPage() {
+//        return "This is the landing page!";
+//    }
+
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
+}
---
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("/add/{firstNumber}/and/{secondNumber}")
+    @ResponseBody
+    public int add(@PathVariable int firstNumber, @PathVariable int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    @GetMapping("/subtract/{firstNumber}/from/{secondNumber}")
+    @ResponseBody
+    public int subtract(@PathVariable int firstNumber, @PathVariable int secondNumber) {
+        return secondNumber - firstNumber;
+    }
+
+    @GetMapping("/multiply/{firstNumber}/and/{secondNumber}")
+    @ResponseBody
+    public int multiply(@PathVariable int firstNumber, @PathVariable int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    @GetMapping("/divide/{firstNumber}/by/{secondNumber}")
+    @ResponseBody
+    public int divide(@PathVariable int firstNumber, @PathVariable int secondNumber) {
+        return secondNumber / firstNumber;
+    }
+}
